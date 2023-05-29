cca189367c6cf72963ecc75af46426ccbecb8ddd
Himanshu Thakur <champhimu359@gmail.com>
2023-05-29 07:42:11 +0530

    package updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import Home from './component/Home'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    {/* <BrowserRouter>
-    <Routes>
-      <Route path='/' exact element={<Home />} />  
-    </Routes>
-    </BrowserRouter> */}
     <Home />
     </>
   );
